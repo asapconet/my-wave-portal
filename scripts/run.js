7a9c1fe8ed1286c8b8f1837fd6f9e231fb59bf04
asapconet <aaronrickymeek@gmail.com>
2022-06-04 00:47:53 +0100

    Feat: updated test file for new smart contract

--- scripts/run.js
+++ scripts/run.js
@@ -1,26 +1,41 @@
 const inDisplay = (args) => console.log(args); //my easy console func
 
 const major = async () => {
-  const [_, randomPerson] = await hre.ethers.getSigners();
+  // const [_, randomPerson] = await hre.ethers.getSigners();
   const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
-  const waveContract = await waveContractFactory.deploy();
-  await waveContract.deployed();
+  const waveContract = await waveContractFactory.deploy({
+    value: hre.ethers.utils.parseEther("0.1"),
+  });
 
+  await waveContract.deployed();
   inDisplay("This is the contract address:" + waveContract.address);
   // inDisplay("And the contract was deployed by:" + owner.address);
 
-  let waveCount;
-  waveCount = await waveContract.getTotalWaves();
-  inDisplay(waveCount.toNumber());
+  //Checking wallet balance
+  let contractBalance = await hre.ethers.provider.getBalance(
+    waveContract.address
+  );
+  inDisplay("Contract Balance:", hre.ethers.utils.formatEther(contractBalance));
+
+  // let waveCount;
+  // waveCount = await waveContract.getTotalWaves();
+  // inDisplay(waveCount.toNumber());
 
   // for sending waves
   let waveTransaction = await waveContract.wave("Message here");
   await waveTransaction.wait(); // while transation is minned
 
-  waveTransaction = await waveContract
-    .connect(randomPerson)
-    .wave("The next message right here");
-  await waveTransaction.wait();  
+  //getting current balance contract to be sure a trans was held or not
+  contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+  inDisplay(
+    "Available Contract Balance:",
+    hre.ethers.utils.formatEther(contractBalance)
+  );
+
+  // waveTransaction = await waveContract
+  //   .connect(randomPerson)
+  //   .wave("The next message right here");
+  // await waveTransaction.wait();
 
   let allWaves = await waveContract.getAllWaves();
   inDisplay(allWaves);
