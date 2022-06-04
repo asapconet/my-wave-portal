4d9c646881f0f92e61c580f35b30a6cbe2814a48
asapconet <aaronrickymeek@gmail.com>
2022-06-04 01:46:49 +0100

    Update: added the retake time to 5mins

--- scripts/run.js
+++ scripts/run.js
@@ -8,24 +8,21 @@ const major = async () => {
   });
 
   await waveContract.deployed();
-  inDisplay("This is the contract address:" + waveContract.address);
+  inDisplay("This is the contract address:", waveContract.address);
   // inDisplay("And the contract was deployed by:" + owner.address);
 
   //Checking wallet balance
   let contractBalance = await hre.ethers.provider.getBalance(
     waveContract.address
   );
-  inDisplay(
-    "Available Contract balance:",
-    hre.ethers.utils.formatEther(contractBalance)
-  );
+  inDisplay("Contract balance:", hre.ethers.utils.formatEther(contractBalance));
 
   // let waveCount;
   // waveCount = await waveContract.getTotalWaves();
   // inDisplay(waveCount.toNumber());
 
   // for sending waves
-  let waveTransaction = await waveContract.wave("Message here", "");
+  let waveTransaction = await waveContract.wave("Message here");
   await waveTransaction.wait(); // while transation is minned
 
   //getting current balance contract to be sure a trans was held or not
